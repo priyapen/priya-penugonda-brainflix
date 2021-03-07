358d56ffdd5b3e751d9008a4cfea38700e680c21
Priya Penugonda <priya.penugonda@gmail.com>
2021-03-07 18:58:26 -0500

    complete functionality for upload button navigation to a separate page

--- sprint-two/src/Components/Pages/UploadPage.js
+++ sprint-two/src/Components/Pages/UploadPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './UploadPage.scss';
+import thumbnail from '../../Assets/Images/Upload-video-preview.jpg';
 
 
 export default function UploadPage (props) {
@@ -11,18 +12,21 @@ export default function UploadPage (props) {
    }
     
     return (
-        <div>
+        <section className="upl">
             <h1 className="upl__heading">Upload Video</h1>
             <p className="upl__thumb">Video Thumbnail</p>
-            <form action="">
+            <img src={thumbnail} className="upl__img"/>
+            <form action="" className="upl__form">
                 <label htmlFor="" className="upl__titlelbl">Title your video</label>
                 <input type="text" placeholder="Add a title to your video" className="upl__title" name="vidtitle"></input>
                 <label htmlFor="" className="upl__desclbl">Add a video description</label>
                <textarea placeholder="Add a description of your video" className="upl__desc"></textarea>
+            </form>
+            <div className="upl__buttons">
                 <p className="upl__publbtn" onClick={ clickedwithinupload}>Publish</p>
                 <p className="upl__cancel" onClick={()=>{props.history.goBack();}}>Cancel</p>
-            </form>
-        </div>
+            </div>
+        </section>
     );
 };
 
