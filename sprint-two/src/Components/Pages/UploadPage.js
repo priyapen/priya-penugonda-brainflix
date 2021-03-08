aa4b1d9081ba0485028b23f5855b604e02fdefd7
Priya Penugonda <priya.penugonda@gmail.com>
2021-03-07 19:25:40 -0500

    complete mobile styling for upload page

--- sprint-two/src/Components/Pages/UploadPage.js
+++ sprint-two/src/Components/Pages/UploadPage.js
@@ -14,14 +14,18 @@ export default function UploadPage (props) {
     return (
         <section className="upl">
             <h1 className="upl__heading">Upload Video</h1>
-            <p className="upl__thumb">Video Thumbnail</p>
-            <img src={thumbnail} className="upl__img"/>
-            <form action="" className="upl__form">
-                <label htmlFor="" className="upl__titlelbl">Title your video</label>
-                <input type="text" placeholder="Add a title to your video" className="upl__title" name="vidtitle"></input>
-                <label htmlFor="" className="upl__desclbl">Add a video description</label>
-               <textarea placeholder="Add a description of your video" className="upl__desc"></textarea>
-            </form>
+            <div className="upl__flex">
+                <div className="upl__imgblck">   
+                    <p className="upl__thumb">Video Thumbnail</p>
+                    <img src={thumbnail} className="upl__img" />
+                 </div>
+                <form action="" className="upl__form">
+                    <label htmlFor="" className="upl__titlelbl">Title your video
+                    <input type="text" placeholder="Add a title to your video" className="upl__title" name="vidtitle"></input></label>
+                    <label htmlFor="" className="upl__desclbl">Add a video description
+                    <textarea placeholder="Add a description of your video" className="upl__desc"></textarea></label>
+                </form>
+            </div>
             <div className="upl__buttons">
                 <p className="upl__publbtn" onClick={ clickedwithinupload}>Publish</p>
                 <p className="upl__cancel" onClick={()=>{props.history.goBack();}}>Cancel</p>
